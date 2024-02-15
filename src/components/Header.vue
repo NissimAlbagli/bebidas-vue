5049c65afe9bea5f893c0cf56f9757d6e17f34b4
Nissim Albagli <nissimalbagli@gmail.com>
2024-02-15 18:34:06 -0300

    Se separo el url del request en servicios y api para hacer mas facil el mantenimiento y se crearon funciones

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,12 @@
     const route = useRoute();
     const store = useBebidasStore();
 
-    const paginaInicio = computed(() => route.name === 'inicio')
+    const paginaInicio = computed(() => route.name === 'inicio');
+
+    const handleSubmit = () => {
+        // TODO: Validar
+        store.obtenerRecetas()
+    }
 </script>
 
 <template>
@@ -48,6 +53,7 @@
             <form 
                 class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
                 v-if="paginaInicio"
+                @submit.prevent="handleSubmit"
             >
             <div class="space-y-4">
                 <label 
@@ -58,6 +64,7 @@
                     type="text"
                     class="p-3 w-full rounded-lg focus:outline-none"
                     placeholder="Nombre o Ingrediente: ej. Vodka, Tequila, etc..."
+                    v-model="store.busqueda.nombre"
                 >
             </div>
 
@@ -68,6 +75,7 @@
                 <select 
                     id="categoria"
                     class="p-3 w-full rounded-lg focus:outline-none"
+                    v-model="store.busqueda.categoria"
                 >
                     <option value="">-- Seleccione --</option>
                     <option 
