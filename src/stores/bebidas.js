30b4d2ec3c2b3f8b26aa456ebd8bccb29a03f5f5
Nissim Albagli <nissimalbagli@gmail.com>
2024-02-15 21:48:17 -0300

    Se agrego logica para conseguir la informacion de una receta por su id

--- src/stores/bebidas.js
+++ src/stores/bebidas.js
@@ -10,6 +10,7 @@ export const useBebidasStore = defineStore('bebidas', () => {
       categoria: '',
      })
      const recetas = ref([]);
+     
 
      onMounted(async () => {
         const {data: {drinks}} = await APIService.obtenerCategorias();
@@ -22,10 +23,16 @@ export const useBebidasStore = defineStore('bebidas', () => {
       recetas.value = drinks;
    }
 
+     async function seleccionarBebida(id) {
+        const {data: {drinks}} = await APIService.buscarReceta(id);
+        console.log(drinks[0]);
+   }
+
      return { 
         categorias,
         busqueda,
         obtenerRecetas,
-        recetas
+        recetas,
+        seleccionarBebida
     }
 })
