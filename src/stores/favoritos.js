e70e6ec02a9a0e2e0e017bec89962677f880dbb5
Nissim Albagli <nissimalbagli@gmail.com>
2024-02-17 18:12:05 -0300

    Se creo un store para favoritos, se creo la funcionalidad para agregar favoritos y se guardan en localStorage

---
+++ src/stores/favoritos.js
@@ -0,0 +1,41 @@
+import { ref, watch, onMounted } from 'vue';
+import { defineStore } from 'pinia'
+import { useBebidasStore } from './bebidas'; 
+
+export const useFavoritosStore = defineStore('favoritos', () => {
+
+    const bebidas = useBebidasStore();
+    const favoritos = ref([]);
+
+    onMounted(() => {
+        favoritos.value = JSON.parse(localStorage.getItem('favoritos')) ?? []
+    })
+
+    watch(favoritos, () => {
+        sincronizarLocalStorage()
+    },{
+        deep: true
+    })
+
+    function sincronizarLocalStorage() {
+        localStorage.setItem('favoritos', JSON.stringify(favoritos.value));
+    }
+
+    function existeFavorito(id) {
+        const favoritosLocalStorage = JSON.parse(localStorage.getItem('favoritos')) ?? []
+        return favoritosLocalStorage.some(favorito => favorito.idDrink === id)
+    }
+
+    function handleClickFavorito() {
+        if(existeFavorito(bebidas.receta.idDrink)) {
+            console.log('Ya Existe...');
+        } else {
+            favoritos.value.push(bebidas.receta);
+        }
+    }
+
+    return {
+        favoritos,
+        handleClickFavorito
+    }
+})
