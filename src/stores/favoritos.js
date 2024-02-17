11be836e5298aa18bff3d2049e15c29bc0f664af
Nissim Albagli <nissimalbagli@gmail.com>
2024-02-17 18:18:25 -0300

    Se agrego un texto condicional dependiendo de si la bebida esta o no en favoritos

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -36,6 +36,7 @@ export const useFavoritosStore = defineStore('favoritos', () => {
 
     return {
         favoritos,
-        handleClickFavorito
+        handleClickFavorito,
+        existeFavorito
     }
 })
